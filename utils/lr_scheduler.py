6c853a980010fdaa3f69a03d6c42c8dc4aaa6ea3
QETJDNHHVNKD <2145123386@qq.com>
2026-01-28 10:19:30 +0800

    Refactor lr_scheduler.py by removing comments
    Removed comments in Chinese and cleaned up formatting.

--- utils/lr_scheduler.py
+++ utils/lr_scheduler.py
@@ -1,9 +1,6 @@
 import torch
 import math
 
-# -----------------------------------------------------------------------------
-# ★ Warmup + Cosine Annealing 学习率调度器
-# -----------------------------------------------------------------------------
 
 class LinearWarmupCosineAnnealingLR(torch.optim.lr_scheduler._LRScheduler):
     def __init__(self, optimizer, warmup_epochs, max_epochs, warmup_start_lr=1e-5, eta_min=1e-6, last_epoch=-1):
@@ -12,16 +9,15 @@ class LinearWarmupCosineAnnealingLR(torch.optim.lr_scheduler._LRScheduler):
         self.warmup_start_lr = warmup_start_lr
         self.eta_min = eta_min
         super().__init__(optimizer, last_epoch)
-
     def get_lr(self):
         if self.last_epoch < self.warmup_epochs:
-            # 线性 warmup
+        
             return [
                 self.warmup_start_lr + (base_lr - self.warmup_start_lr) * self.last_epoch / self.warmup_epochs
                 for base_lr in self.base_lrs
             ]
         else:
-            # 余弦退火
+
             return [
                 self.eta_min + 0.5 * (base_lr - self.eta_min) *
                 (1 + math.cos(math.pi * (self.last_epoch - self.warmup_epochs) / (self.max_epochs - self.warmup_epochs)))
